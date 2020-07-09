b18d1ebc03b4ccd293ac0baf61e7268a26afbe7b
Alben Magsombol <amagsomb@MGC12W426HHV2R.fbpld77.ford.com>
2020-07-09 16:24:28 -0400

    Add swagger doc and fill out read me

---
+++ README.md
@@ -0,0 +1,6 @@
+To run all test including IntegrationTest: ./gradlew test
+E2E Test is in test/com/alben/destinations/integration folder
+To launch: ./gradlew bootRun
+Once launched, access swagger document at: http://localhost:8080/swagger-ui.html#/destinations-controller/connectedUsingGET
+
+Thank you! :D
--- src/main/java/com/alben/destinations/controllers/DestinationsController.java
+++ src/main/java/com/alben/destinations/controllers/DestinationsController.java
@@ -2,6 +2,7 @@ package com.alben.destinations.controllers;
 
 import com.alben.destinations.controllers.models.ConnectedApiResponse;
 import com.alben.destinations.services.DestinationsService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class DestinationsController {
     private DestinationsService destinationsService;
 
     @GetMapping
+    @ApiOperation(value = "Connected API", notes = "Returns 'yes' or 'no' in the result attribute when provided origin and destination locations are connected.")
     public ResponseEntity<ConnectedApiResponse> connected(
             @RequestParam String origin,
             @RequestParam String destination) {
